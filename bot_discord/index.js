d7658345c6868f4082e92aff549946b87c1760b6
Andre Sousa <andre.sousa@reptek.pt>
2021-01-30 02:01:44 +0000

    Got the bot to work!
    But it only sends the events and don't verify for existing one! ;-;

--- bot_discord/index.js
+++ bot_discord/index.js
@@ -6,60 +6,67 @@ const client = new Discord.Client();
 
 let rawdata = fs.readFileSync('../dados.json');
 const data = JSON.parse(rawdata)
-const mucrocyAPI_URL = "http://localhost/api/servidor/logs"
-const battleMetrics_TOKEN = data['BATTLEMETRICS_TOKEN']
+const mucrocyAPI_URL = "http://85.234.128.69/api/servidor/logs"
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 
-function verifyIfExists(array1, array2){
-    let rArray = new Array()
-    for (let i = 0; i < array1.length; i++) {
-        for (let j = 0; j < array2.length; j++) {
-            const element = array1[i];
-            const elementt = array2[j];
-            if(element == elementt){
-                console.log(element);
+function verifyIfEquals(array1, array2){
+    if(array1.length != array2.length) return false
 
-            }
-            // rArray.push(elementt)
-            // if(element.textContent == elementt){
-            //     rArray.pop();
-            // }
-            
-        }
+    for (var i = 0; array1.length < i; i++){
+        if (array1[i] != array2[i]) return false;
     }
-
-    return "a";
+    
+    return true
 }
 
-
 client.on('message', msg => {
-    if (msg.content === 'ping') {
-        let array2 = new Array()
-        let array3 = new Array()
-        request({
-            url: mucrocyAPI_URL,
-            headers: {
-                'Authorization': `Bearer ${battleMetrics_TOKEN}`
-            },
-            rejectUnauthorized: false
+    let eventos = new Array()
+    let array = new Array()
+    if (msg.content === 'quero logs puta') {
+        msg.channel.send("Ok babe, estou a mandar logs", {files:["../fotos/IMG_1443.PNG"]})
+        setTimeout(() => {msg.channel.send("OOPS MANDEI A COISA ERRADA")}, 3000)
+        setTimeout(() => {msg.channel.send("A enviar.....")}, 3000)
+        setInterval(() => {
+            request({
+                url: mucrocyAPI_URL,
+                rejectUnauthorized: false
+            
+            }, function(err, response) {
+                if(err) {
+                    console.error(err);
+                } else {
+                    const body = response.body
+                    const bodyJson = JSON.parse(body)
+                    eventos = bodyJson.Eventos
+                    setTimeout(() =>{
+                        request({
+                            url: mucrocyAPI_URL,
+                            rejectUnauthorized: false
+                        
+                        }, function(err, response) {
+                            if(err) {
+                                console.error(err);
+                            } else {
+                                array = JSON.parse(response.body).Eventos
+                                msg.channel.send("\u200B")
+                                if(verifyIfEquals(eventos, array)){
+                                    for(let i = 0; i < array.length-1; i++){
+                                        msg.channel.send(array[i])
+                                    }
+                                }
+                            }
+                        
+                        });
+                    }, 5000)
+                }
+            
+            });
+        }, 10000)
         
-        }, function(err, response) {
-              if(err) {
-                console.error(err);
-              } else {
-                  const body = response.body
-                  const bodyJson = JSON.parse(body)
-                  // array2 = verifyIfExists(array1.Eventos, )
-                  // let t = verifyifexists(array3.evento, array1.eventos);
-
-                  console.log(bodyJson.Eventos)
-              }
-        
-        });
     }
   });
 
